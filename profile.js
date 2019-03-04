e52a2c5c76c432b4944cb29850143cdc1e57e00c
Eddie Chu <contact@eddiechu.me>
2019-03-03 22:21:47 -0800

    moved viewProfile to find.js

--- profile.js
+++ profile.js
@@ -54,6 +54,29 @@ function populateProfile(arr) {
     $('#ownPosts').html(items);
 }
 
+//AJAX function to view posts owned by Logged in User, and posts joined by Logged in user, triggered by click event on nav button My Profile
+function viewProfile() {
+    const base = 'http://localhost:8080/api/teams/';
+    const localtoken = localStorage.getItem('localtoken');
+    const currentUserId = localStorage.getItem('currentUser');
+    const url = base + currentUserId;
+    console.log(url);
+    return fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${localtoken}`
+        }
+    })
+        .then(res => res.json())
+        .then(response => {
+            console.log('find profile triggered');
+            console.log(response);
+            populateProfile(response);
+        })
+        .catch(err => console.log(err));
+}
+
 //Triggered by clicking a game post on profile page. Start of chain >> ViewSinglePost >> modalizePostProfile >> End.
 function popPost() {
     $('#ownPosts').on('click', '.post-item', (event) => {
--- script.js
+++ script.js
@@ -177,28 +177,7 @@ function registerProfile() {
 
 
 
-//AJAX function to view posts owned by Logged in User, and posts joined by Logged in user, triggered by click event on nav button My Profile
-function viewProfile() {
-    const base = 'http://localhost:8080/api/teams/';
-    const localtoken = localStorage.getItem('localtoken');
-    const currentUserId = localStorage.getItem('currentUser');
-    const url = base + currentUserId;
-    console.log(url);
-    return fetch(url, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${localtoken}`
-        }
-    })
-    .then(res => res.json())
-    .then(response => {
-        console.log('find profile triggered');
-        console.log(response);
-        populateProfile(response);
-    })
-    .catch(err => console.log(err));
-}
+
 
 
 
